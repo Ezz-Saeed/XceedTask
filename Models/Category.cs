d53f1a80319355c7e9356386749d419a33495b05
Ezz Saeed <ezzsaid299@gmail.com>
2024-12-11 04:35:58 -0800

    Creating models of Product, User and Category

---
+++ Models/Category.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace XceedTask.Models
+{
+    public class Category
+    {
+        [Key]
+        public string Id { get; set; }
+        [Required]
+        [MaxLength(200)]
+        public string Name { get; set; }
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace XceedTask.Models
+{
+    public class Product
+    {
+        [Key]
+        public string Id { get; set; }
+        [Required]
+        [MaxLength(200)]
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public DateTime CreationDate { get; set; } = DateTime.Now;
+        public DateTime StartDate { get; set; }
+        public TimeSpan Duration { get; set; }
+        [ForeignKey(nameof(User))]
+        public string UserId { get; set; }
+        public AppUser User { get; set; }
+        [ForeignKey(nameof(Category))]
+        public string CategoryId { get; set; }
+        public Category Category { get; set; }
+
+    }
+}
